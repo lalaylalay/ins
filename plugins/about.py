d25494e5117105a2907b038d36d331d6b930a14a
lalaylalay <36571487+lalaylalay@users.noreply.github.com>
2018-02-17 12:41:26 -0500

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -5,7 +5,7 @@ from message import Message
 @asyncio.coroutine
 def run(message, matches, chat_id, step):
     return [Message(chat_id).set_text(
-        "Siarobo Developed By *Siyanew*\n_For further information join_ @siyanews _Channel!_", parse_mode="markdown")]
+        "*Instagram* post downloader Bot", parse_mode="markdown")]
 
 
 plugin = {
